901edc09bf1693b46cd39b976429482ad5dff33f
Willian-Louback <williandlouback@gmail.com>
2023-02-20 21:14:15 -0300

    add new things

--- commands/teste.js
+++ commands/teste.js
@@ -2,15 +2,13 @@ require('dotenv').config({ path: './secure/.env'});
 
 module.exports = {
     name: 'help',
-    execute: (txt) => {
+    execute: (bot, txt) => {
         const user = txt.from;
-        if(user.id == process.env.ID_CRIADOR){
-            txt.reply(`Olá ${user.first_name}, é sempre um prazer ver você. Meu criador, como posso ajudá-lo?`);
-        } else {
-            if(user.first_name == "Rian"){
-                console.log(user.id);
-            }
-            txt.reply(`Olá ${user.first_name} ${user.last_name}, como posso ajudá-lo?`);
-        }
+        txt.reply(
+            `Olá, ${user.name}. Estou aqui para ajudar!\n`+
+            `Lista de comandos:\n`+
+            ` /help - Use este comando para obter ajuda, não exite em perguntar!\n`+
+            ` /start - Use este comando para começarmos!\n`
+        )
     }
 }
