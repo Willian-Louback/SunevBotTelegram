60ce76e01480e94a0bcb20518b75faf29bd75dfc
Willian-Louback <williandlouback@gmail.com>
2023-02-23 17:44:34 -0300

    new command

--- commands/perguntas.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "perguntar",
-    execute: async (bot, txt) => {
-        await txt.reply(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/6.gif`);
-    }
-}
---
+++ commands/userInfo.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "userInfo",
+    execute: async (bot, txt) => {
+        const user = txt.from;
+        await txt.reply(`Nome: ${user.first_name}.\n`+
+        `Sobrenome: ${user.last_name}.\n`+
+        `Username: ${user.username}.\n`+
+        `ID: ${user.id}.`)
+    }
+}
